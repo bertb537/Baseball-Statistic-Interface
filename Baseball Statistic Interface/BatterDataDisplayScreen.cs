2176f8ca04d5ea7cc0a8f88e640f83b26c84dfcc
Brett Bertrand <brett.bertrand@frontier.com>
2019-11-18 15:36:09 -0800

    Batter Data Display Screen 1: Created

--- Baseball Statistic Interface/BatterDataDisplayScreen.cs
+++ Baseball Statistic Interface/BatterDataDisplayScreen.cs
@@ -10,24 +10,24 @@ using System.Windows.Forms;
 
 namespace Baseball_Statistic_Interface
 {
-    public partial class PitcherDataDisplayScreen : UserControl
+    public partial class BatterDataDisplayScreen : UserControl
     {
-        public PitcherDataDisplayScreen()
+        public BatterDataDisplayScreen()
         {
             InitializeComponent();
         }
 
+        private void OPS_LABEL_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BatterDataDisplayScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+
         private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
-        {
-
-        }
-
-        private void PitcherDataDisplayScreen_Load(object sender, EventArgs e)
         {
 
         }
--- Baseball Statistic Interface/PitcherDataDisplayScreen.Designer.cs
+++ Baseball Statistic Interface/PitcherDataDisplayScreen.Designer.cs
@@ -30,23 +30,23 @@
         {
             this.PLAYER_NAME_LABEL = new System.Windows.Forms.Label();
             this.LEFT_RIGHT_HAND_BATTER = new System.Windows.Forms.SplitContainer();
+            this.LEFT_O_TWO_DATAGRIDVIEW = new System.Windows.Forms.DataGridView();
+            this.LEFT_OPENER_DATAGRIDVIEW = new System.Windows.Forms.DataGridView();
+            this.LEFT_HAND_BATTER_LABEL = new System.Windows.Forms.Label();
+            this.RIGHT_O_TWO_DATAGRIDVIEW = new System.Windows.Forms.DataGridView();
+            this.RIGHT_OPENER_DATAGRIDVIEW = new System.Windows.Forms.DataGridView();
+            this.RIGHT_HAND_BATTER_LABEL = new System.Windows.Forms.Label();
             this.THROWING_ARM_LABEL = new System.Windows.Forms.Label();
             this.ERA_LABEL = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.LEFT_HAND_BATTER_LABEL = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.LEFT_OPENER_DATAGRIDVIEW = new System.Windows.Forms.DataGridView();
-            this.LEFT_O_TWO_DATAGRIDVIEW = new System.Windows.Forms.DataGridView();
-            this.RIGHT_OPENER_DATAGRIDVIEW = new System.Windows.Forms.DataGridView();
-            this.RIGHT_O_TWO_DATAGRIDVIEW = new System.Windows.Forms.DataGridView();
+            this.MA_LABEL = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.LEFT_RIGHT_HAND_BATTER)).BeginInit();
             this.LEFT_RIGHT_HAND_BATTER.Panel1.SuspendLayout();
             this.LEFT_RIGHT_HAND_BATTER.Panel2.SuspendLayout();
             this.LEFT_RIGHT_HAND_BATTER.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.LEFT_OPENER_DATAGRIDVIEW)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.LEFT_O_TWO_DATAGRIDVIEW)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.RIGHT_OPENER_DATAGRIDVIEW)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LEFT_OPENER_DATAGRIDVIEW)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.RIGHT_O_TWO_DATAGRIDVIEW)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RIGHT_OPENER_DATAGRIDVIEW)).BeginInit();
             this.SuspendLayout();
             // 
             // PLAYER_NAME_LABEL
@@ -75,11 +75,63 @@
             // 
             this.LEFT_RIGHT_HAND_BATTER.Panel2.Controls.Add(this.RIGHT_O_TWO_DATAGRIDVIEW);
             this.LEFT_RIGHT_HAND_BATTER.Panel2.Controls.Add(this.RIGHT_OPENER_DATAGRIDVIEW);
-            this.LEFT_RIGHT_HAND_BATTER.Panel2.Controls.Add(this.label1);
+            this.LEFT_RIGHT_HAND_BATTER.Panel2.Controls.Add(this.RIGHT_HAND_BATTER_LABEL);
             this.LEFT_RIGHT_HAND_BATTER.Size = new System.Drawing.Size(1005, 707);
             this.LEFT_RIGHT_HAND_BATTER.SplitterDistance = 500;
             this.LEFT_RIGHT_HAND_BATTER.TabIndex = 1;
             this.LEFT_RIGHT_HAND_BATTER.SplitterMoved += new System.Windows.Forms.SplitterEventHandler(this.splitContainer1_SplitterMoved);
+            // 
+            // LEFT_O_TWO_DATAGRIDVIEW
+            // 
+            this.LEFT_O_TWO_DATAGRIDVIEW.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.LEFT_O_TWO_DATAGRIDVIEW.Location = new System.Drawing.Point(3, 384);
+            this.LEFT_O_TWO_DATAGRIDVIEW.Name = "LEFT_O_TWO_DATAGRIDVIEW";
+            this.LEFT_O_TWO_DATAGRIDVIEW.Size = new System.Drawing.Size(494, 256);
+            this.LEFT_O_TWO_DATAGRIDVIEW.TabIndex = 2;
+            // 
+            // LEFT_OPENER_DATAGRIDVIEW
+            // 
+            this.LEFT_OPENER_DATAGRIDVIEW.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.LEFT_OPENER_DATAGRIDVIEW.Location = new System.Drawing.Point(3, 64);
+            this.LEFT_OPENER_DATAGRIDVIEW.Name = "LEFT_OPENER_DATAGRIDVIEW";
+            this.LEFT_OPENER_DATAGRIDVIEW.Size = new System.Drawing.Size(494, 256);
+            this.LEFT_OPENER_DATAGRIDVIEW.TabIndex = 1;
+            // 
+            // LEFT_HAND_BATTER_LABEL
+            // 
+            this.LEFT_HAND_BATTER_LABEL.AutoSize = true;
+            this.LEFT_HAND_BATTER_LABEL.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LEFT_HAND_BATTER_LABEL.Location = new System.Drawing.Point(158, 19);
+            this.LEFT_HAND_BATTER_LABEL.Name = "LEFT_HAND_BATTER_LABEL";
+            this.LEFT_HAND_BATTER_LABEL.Size = new System.Drawing.Size(169, 25);
+            this.LEFT_HAND_BATTER_LABEL.TabIndex = 0;
+            this.LEFT_HAND_BATTER_LABEL.Text = "Left-Hand Batter";
+            // 
+            // RIGHT_O_TWO_DATAGRIDVIEW
+            // 
+            this.RIGHT_O_TWO_DATAGRIDVIEW.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.RIGHT_O_TWO_DATAGRIDVIEW.Location = new System.Drawing.Point(3, 384);
+            this.RIGHT_O_TWO_DATAGRIDVIEW.Name = "RIGHT_O_TWO_DATAGRIDVIEW";
+            this.RIGHT_O_TWO_DATAGRIDVIEW.Size = new System.Drawing.Size(495, 256);
+            this.RIGHT_O_TWO_DATAGRIDVIEW.TabIndex = 3;
+            // 
+            // RIGHT_OPENER_DATAGRIDVIEW
+            // 
+            this.RIGHT_OPENER_DATAGRIDVIEW.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.RIGHT_OPENER_DATAGRIDVIEW.Location = new System.Drawing.Point(3, 64);
+            this.RIGHT_OPENER_DATAGRIDVIEW.Name = "RIGHT_OPENER_DATAGRIDVIEW";
+            this.RIGHT_OPENER_DATAGRIDVIEW.Size = new System.Drawing.Size(495, 256);
+            this.RIGHT_OPENER_DATAGRIDVIEW.TabIndex = 2;
+            // 
+            // RIGHT_HAND_BATTER_LABEL
+            // 
+            this.RIGHT_HAND_BATTER_LABEL.AutoSize = true;
+            this.RIGHT_HAND_BATTER_LABEL.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RIGHT_HAND_BATTER_LABEL.Location = new System.Drawing.Point(157, 19);
+            this.RIGHT_HAND_BATTER_LABEL.Name = "RIGHT_HAND_BATTER_LABEL";
+            this.RIGHT_HAND_BATTER_LABEL.Size = new System.Drawing.Size(183, 25);
+            this.RIGHT_HAND_BATTER_LABEL.TabIndex = 1;
+            this.RIGHT_HAND_BATTER_LABEL.Text = "Right-Hand Batter";
             // 
             // THROWING_ARM_LABEL
             // 
@@ -101,89 +153,38 @@
             this.ERA_LABEL.TabIndex = 3;
             this.ERA_LABEL.Text = "ERA: .xxx";
             // 
-            // label3
+            // MA_LABEL
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(897, 23);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(89, 25);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "MA: xxx";
-            // 
-            // LEFT_HAND_BATTER_LABEL
-            // 
-            this.LEFT_HAND_BATTER_LABEL.AutoSize = true;
-            this.LEFT_HAND_BATTER_LABEL.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LEFT_HAND_BATTER_LABEL.Location = new System.Drawing.Point(158, 19);
-            this.LEFT_HAND_BATTER_LABEL.Name = "LEFT_HAND_BATTER_LABEL";
-            this.LEFT_HAND_BATTER_LABEL.Size = new System.Drawing.Size(169, 25);
-            this.LEFT_HAND_BATTER_LABEL.TabIndex = 0;
-            this.LEFT_HAND_BATTER_LABEL.Text = "Left-Hand Batter";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(157, 19);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(169, 25);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Left-Hand Batter";
-            // 
-            // LEFT_OPENER_DATAGRIDVIEW
-            // 
-            this.LEFT_OPENER_DATAGRIDVIEW.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.LEFT_OPENER_DATAGRIDVIEW.Location = new System.Drawing.Point(3, 64);
-            this.LEFT_OPENER_DATAGRIDVIEW.Name = "LEFT_OPENER_DATAGRIDVIEW";
-            this.LEFT_OPENER_DATAGRIDVIEW.Size = new System.Drawing.Size(494, 256);
-            this.LEFT_OPENER_DATAGRIDVIEW.TabIndex = 1;
-            // 
-            // LEFT_O_TWO_DATAGRIDVIEW
-            // 
-            this.LEFT_O_TWO_DATAGRIDVIEW.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.LEFT_O_TWO_DATAGRIDVIEW.Location = new System.Drawing.Point(3, 384);
-            this.LEFT_O_TWO_DATAGRIDVIEW.Name = "LEFT_O_TWO_DATAGRIDVIEW";
-            this.LEFT_O_TWO_DATAGRIDVIEW.Size = new System.Drawing.Size(494, 256);
-            this.LEFT_O_TWO_DATAGRIDVIEW.TabIndex = 2;
-            // 
-            // RIGHT_OPENER_DATAGRIDVIEW
-            // 
-            this.RIGHT_OPENER_DATAGRIDVIEW.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.RIGHT_OPENER_DATAGRIDVIEW.Location = new System.Drawing.Point(3, 64);
-            this.RIGHT_OPENER_DATAGRIDVIEW.Name = "RIGHT_OPENER_DATAGRIDVIEW";
-            this.RIGHT_OPENER_DATAGRIDVIEW.Size = new System.Drawing.Size(495, 256);
-            this.RIGHT_OPENER_DATAGRIDVIEW.TabIndex = 2;
-            // 
-            // RIGHT_O_TWO_DATAGRIDVIEW
-            // 
-            this.RIGHT_O_TWO_DATAGRIDVIEW.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.RIGHT_O_TWO_DATAGRIDVIEW.Location = new System.Drawing.Point(3, 384);
-            this.RIGHT_O_TWO_DATAGRIDVIEW.Name = "RIGHT_O_TWO_DATAGRIDVIEW";
-            this.RIGHT_O_TWO_DATAGRIDVIEW.Size = new System.Drawing.Size(495, 256);
-            this.RIGHT_O_TWO_DATAGRIDVIEW.TabIndex = 3;
+            this.MA_LABEL.AutoSize = true;
+            this.MA_LABEL.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MA_LABEL.Location = new System.Drawing.Point(897, 23);
+            this.MA_LABEL.Name = "MA_LABEL";
+            this.MA_LABEL.Size = new System.Drawing.Size(89, 25);
+            this.MA_LABEL.TabIndex = 4;
+            this.MA_LABEL.Text = "MA: xxx";
             // 
             // PitcherDataDisplayScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.MA_LABEL);
             this.Controls.Add(this.ERA_LABEL);
             this.Controls.Add(this.THROWING_ARM_LABEL);
             this.Controls.Add(this.LEFT_RIGHT_HAND_BATTER);
             this.Controls.Add(this.PLAYER_NAME_LABEL);
             this.Name = "PitcherDataDisplayScreen";
             this.Size = new System.Drawing.Size(1011, 779);
+            this.Load += new System.EventHandler(this.PitcherDataDisplayScreen_Load);
             this.LEFT_RIGHT_HAND_BATTER.Panel1.ResumeLayout(false);
             this.LEFT_RIGHT_HAND_BATTER.Panel1.PerformLayout();
             this.LEFT_RIGHT_HAND_BATTER.Panel2.ResumeLayout(false);
             this.LEFT_RIGHT_HAND_BATTER.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.LEFT_RIGHT_HAND_BATTER)).EndInit();
             this.LEFT_RIGHT_HAND_BATTER.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.LEFT_OPENER_DATAGRIDVIEW)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.LEFT_O_TWO_DATAGRIDVIEW)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.RIGHT_OPENER_DATAGRIDVIEW)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LEFT_OPENER_DATAGRIDVIEW)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.RIGHT_O_TWO_DATAGRIDVIEW)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RIGHT_OPENER_DATAGRIDVIEW)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -195,9 +196,9 @@
         private System.Windows.Forms.SplitContainer LEFT_RIGHT_HAND_BATTER;
         private System.Windows.Forms.Label THROWING_ARM_LABEL;
         private System.Windows.Forms.Label ERA_LABEL;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label MA_LABEL;
         private System.Windows.Forms.Label LEFT_HAND_BATTER_LABEL;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label RIGHT_HAND_BATTER_LABEL;
         private System.Windows.Forms.DataGridView LEFT_O_TWO_DATAGRIDVIEW;
         private System.Windows.Forms.DataGridView LEFT_OPENER_DATAGRIDVIEW;
         private System.Windows.Forms.DataGridView RIGHT_O_TWO_DATAGRIDVIEW;

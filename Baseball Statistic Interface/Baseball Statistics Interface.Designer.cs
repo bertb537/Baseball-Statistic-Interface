d98891aac8fd5271ebecde21eec99fa442d4f7b9
Brett Bertrand <brett.bertrand@frontier.com>
2019-11-17 16:12:29 -0800

    Home Select Screen: Form Created

---
+++ Baseball Statistic Interface/Baseball Statistics Interface.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Baseball_Statistic_Interface
+{
+    partial class Baseball_Statistics_Interface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.NEW_PLATE_APPEARANCE_BUTTON = new System.Windows.Forms.Button();
+            this.NEW_MOUND_APPEARANCE_BUTTON = new System.Windows.Forms.Button();
+            this.BATTER_DATA_BUTTON = new System.Windows.Forms.Button();
+            this.PITCHER_DATA_BUTTON = new System.Windows.Forms.Button();
+            this.GENERAL_TEAM_DATA_BUTTON = new System.Windows.Forms.Button();
+            this.TEAM_SELECT_COMBOBOX = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.NEW_PLATE_APPEARANCE_BUTTON);
+            this.panel1.Controls.Add(this.NEW_MOUND_APPEARANCE_BUTTON);
+            this.panel1.Controls.Add(this.BATTER_DATA_BUTTON);
+            this.panel1.Controls.Add(this.PITCHER_DATA_BUTTON);
+            this.panel1.Controls.Add(this.GENERAL_TEAM_DATA_BUTTON);
+            this.panel1.Controls.Add(this.TEAM_SELECT_COMBOBOX);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(188, 779);
+            this.panel1.TabIndex = 13;
+            // 
+            // NEW_PLATE_APPEARANCE_BUTTON
+            // 
+            this.NEW_PLATE_APPEARANCE_BUTTON.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.NEW_PLATE_APPEARANCE_BUTTON.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NEW_PLATE_APPEARANCE_BUTTON.Location = new System.Drawing.Point(12, 359);
+            this.NEW_PLATE_APPEARANCE_BUTTON.Name = "NEW_PLATE_APPEARANCE_BUTTON";
+            this.NEW_PLATE_APPEARANCE_BUTTON.Size = new System.Drawing.Size(163, 71);
+            this.NEW_PLATE_APPEARANCE_BUTTON.TabIndex = 17;
+            this.NEW_PLATE_APPEARANCE_BUTTON.Text = "New Plate Appearance";
+            this.NEW_PLATE_APPEARANCE_BUTTON.UseVisualStyleBackColor = false;
+            // 
+            // NEW_MOUND_APPEARANCE_BUTTON
+            // 
+            this.NEW_MOUND_APPEARANCE_BUTTON.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.NEW_MOUND_APPEARANCE_BUTTON.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NEW_MOUND_APPEARANCE_BUTTON.Location = new System.Drawing.Point(12, 282);
+            this.NEW_MOUND_APPEARANCE_BUTTON.Name = "NEW_MOUND_APPEARANCE_BUTTON";
+            this.NEW_MOUND_APPEARANCE_BUTTON.Size = new System.Drawing.Size(163, 71);
+            this.NEW_MOUND_APPEARANCE_BUTTON.TabIndex = 16;
+            this.NEW_MOUND_APPEARANCE_BUTTON.Text = "New Mound Appearance";
+            this.NEW_MOUND_APPEARANCE_BUTTON.UseVisualStyleBackColor = false;
+            // 
+            // BATTER_DATA_BUTTON
+            // 
+            this.BATTER_DATA_BUTTON.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.BATTER_DATA_BUTTON.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BATTER_DATA_BUTTON.Location = new System.Drawing.Point(12, 205);
+            this.BATTER_DATA_BUTTON.Name = "BATTER_DATA_BUTTON";
+            this.BATTER_DATA_BUTTON.Size = new System.Drawing.Size(163, 71);
+            this.BATTER_DATA_BUTTON.TabIndex = 15;
+            this.BATTER_DATA_BUTTON.Text = "Batter Data";
+            this.BATTER_DATA_BUTTON.UseVisualStyleBackColor = false;
+            // 
+            // PITCHER_DATA_BUTTON
+            // 
+            this.PITCHER_DATA_BUTTON.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.PITCHER_DATA_BUTTON.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PITCHER_DATA_BUTTON.Location = new System.Drawing.Point(12, 128);
+            this.PITCHER_DATA_BUTTON.Name = "PITCHER_DATA_BUTTON";
+            this.PITCHER_DATA_BUTTON.Size = new System.Drawing.Size(163, 71);
+            this.PITCHER_DATA_BUTTON.TabIndex = 14;
+            this.PITCHER_DATA_BUTTON.Text = "Pitcher Data";
+            this.PITCHER_DATA_BUTTON.UseVisualStyleBackColor = false;
+            // 
+            // GENERAL_TEAM_DATA_BUTTON
+            // 
+            this.GENERAL_TEAM_DATA_BUTTON.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.GENERAL_TEAM_DATA_BUTTON.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GENERAL_TEAM_DATA_BUTTON.Location = new System.Drawing.Point(12, 51);
+            this.GENERAL_TEAM_DATA_BUTTON.Name = "GENERAL_TEAM_DATA_BUTTON";
+            this.GENERAL_TEAM_DATA_BUTTON.Size = new System.Drawing.Size(163, 71);
+            this.GENERAL_TEAM_DATA_BUTTON.TabIndex = 13;
+            this.GENERAL_TEAM_DATA_BUTTON.Text = "General Team Data";
+            this.GENERAL_TEAM_DATA_BUTTON.UseVisualStyleBackColor = false;
+            // 
+            // TEAM_SELECT_COMBOBOX
+            // 
+            this.TEAM_SELECT_COMBOBOX.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TEAM_SELECT_COMBOBOX.FormattingEnabled = true;
+            this.TEAM_SELECT_COMBOBOX.Location = new System.Drawing.Point(12, 12);
+            this.TEAM_SELECT_COMBOBOX.Name = "TEAM_SELECT_COMBOBOX";
+            this.TEAM_SELECT_COMBOBOX.Size = new System.Drawing.Size(163, 28);
+            this.TEAM_SELECT_COMBOBOX.TabIndex = 12;
+            this.TEAM_SELECT_COMBOBOX.Text = "Team Select";
+            // 
+            // Baseball_Statistics_Interface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1205, 779);
+            this.Controls.Add(this.panel1);
+            this.Name = "Baseball_Statistics_Interface";
+            this.Text = "Baseball_Statistics_Interface";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button NEW_PLATE_APPEARANCE_BUTTON;
+        private System.Windows.Forms.Button NEW_MOUND_APPEARANCE_BUTTON;
+        private System.Windows.Forms.Button BATTER_DATA_BUTTON;
+        private System.Windows.Forms.Button PITCHER_DATA_BUTTON;
+        private System.Windows.Forms.Button GENERAL_TEAM_DATA_BUTTON;
+        private System.Windows.Forms.ComboBox TEAM_SELECT_COMBOBOX;
+    }
+}

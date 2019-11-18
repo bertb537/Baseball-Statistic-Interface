91fddc8167124b369c90346bc858cf98b0365761
Brett Bertrand <brett.bertrand@frontier.com>
2019-11-18 15:58:34 -0800

    Mound Appearance Entry Screen: Created

---
+++ Baseball Statistic Interface/MoundAppearanceEntryScreen.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Baseball_Statistic_Interface
+{
+    partial class MoundAppearanceEntryScreen
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SUBMIT_BUTTON = new System.Windows.Forms.Button();
+            this.SELECT_PITCHER_LABEL = new System.Windows.Forms.Label();
+            this.PITCHER_SELECT_COMBOBOX = new System.Windows.Forms.ComboBox();
+            this.BATTER_LABEL = new System.Windows.Forms.Label();
+            this.LEFT_CHECKBOX = new System.Windows.Forms.CheckBox();
+            this.RIGHT_CHECKBOX = new System.Windows.Forms.CheckBox();
+            this.OPENER_LABEL = new System.Windows.Forms.Label();
+            this.OPENER_TEXTBOX = new System.Windows.Forms.TextBox();
+            this.OPENER_QUADRANT_TEXTBOX = new System.Windows.Forms.TextBox();
+            this.OPENER_QUADRANT_LABEL = new System.Windows.Forms.Label();
+            this.O_TWO_QUADRANT_TEXTBOX = new System.Windows.Forms.TextBox();
+            this.O_TWO_QUADRANT_LABEL = new System.Windows.Forms.Label();
+            this.O_TWO_TEXTBOX = new System.Windows.Forms.TextBox();
+            this.O_TWO_LABEL = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SUBMIT_BUTTON
+            // 
+            this.SUBMIT_BUTTON.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SUBMIT_BUTTON.Location = new System.Drawing.Point(212, 465);
+            this.SUBMIT_BUTTON.Name = "SUBMIT_BUTTON";
+            this.SUBMIT_BUTTON.Size = new System.Drawing.Size(184, 47);
+            this.SUBMIT_BUTTON.TabIndex = 41;
+            this.SUBMIT_BUTTON.Text = "Submit";
+            this.SUBMIT_BUTTON.UseVisualStyleBackColor = true;
+            // 
+            // SELECT_PITCHER_LABEL
+            // 
+            this.SELECT_PITCHER_LABEL.AutoSize = true;
+            this.SELECT_PITCHER_LABEL.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SELECT_PITCHER_LABEL.Location = new System.Drawing.Point(206, 214);
+            this.SELECT_PITCHER_LABEL.Name = "SELECT_PITCHER_LABEL";
+            this.SELECT_PITCHER_LABEL.Size = new System.Drawing.Size(190, 31);
+            this.SELECT_PITCHER_LABEL.TabIndex = 40;
+            this.SELECT_PITCHER_LABEL.Text = "Select Pitcher:";
+            // 
+            // PITCHER_SELECT_COMBOBOX
+            // 
+            this.PITCHER_SELECT_COMBOBOX.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PITCHER_SELECT_COMBOBOX.FormattingEnabled = true;
+            this.PITCHER_SELECT_COMBOBOX.Location = new System.Drawing.Point(402, 212);
+            this.PITCHER_SELECT_COMBOBOX.Name = "PITCHER_SELECT_COMBOBOX";
+            this.PITCHER_SELECT_COMBOBOX.Size = new System.Drawing.Size(258, 33);
+            this.PITCHER_SELECT_COMBOBOX.TabIndex = 39;
+            // 
+            // BATTER_LABEL
+            // 
+            this.BATTER_LABEL.AutoSize = true;
+            this.BATTER_LABEL.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BATTER_LABEL.Location = new System.Drawing.Point(301, 395);
+            this.BATTER_LABEL.Name = "BATTER_LABEL";
+            this.BATTER_LABEL.Size = new System.Drawing.Size(95, 31);
+            this.BATTER_LABEL.TabIndex = 42;
+            this.BATTER_LABEL.Text = "Batter:";
+            // 
+            // LEFT_CHECKBOX
+            // 
+            this.LEFT_CHECKBOX.AutoSize = true;
+            this.LEFT_CHECKBOX.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LEFT_CHECKBOX.Location = new System.Drawing.Point(419, 402);
+            this.LEFT_CHECKBOX.Name = "LEFT_CHECKBOX";
+            this.LEFT_CHECKBOX.Size = new System.Drawing.Size(56, 24);
+            this.LEFT_CHECKBOX.TabIndex = 43;
+            this.LEFT_CHECKBOX.Text = "Left";
+            this.LEFT_CHECKBOX.UseVisualStyleBackColor = true;
+            // 
+            // RIGHT_CHECKBOX
+            // 
+            this.RIGHT_CHECKBOX.AutoSize = true;
+            this.RIGHT_CHECKBOX.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RIGHT_CHECKBOX.Location = new System.Drawing.Point(481, 402);
+            this.RIGHT_CHECKBOX.Name = "RIGHT_CHECKBOX";
+            this.RIGHT_CHECKBOX.Size = new System.Drawing.Size(66, 24);
+            this.RIGHT_CHECKBOX.TabIndex = 45;
+            this.RIGHT_CHECKBOX.Text = "Right";
+            this.RIGHT_CHECKBOX.UseVisualStyleBackColor = true;
+            // 
+            // OPENER_LABEL
+            // 
+            this.OPENER_LABEL.AutoSize = true;
+            this.OPENER_LABEL.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OPENER_LABEL.Location = new System.Drawing.Point(284, 282);
+            this.OPENER_LABEL.Name = "OPENER_LABEL";
+            this.OPENER_LABEL.Size = new System.Drawing.Size(112, 31);
+            this.OPENER_LABEL.TabIndex = 46;
+            this.OPENER_LABEL.Text = "Opener:";
+            // 
+            // OPENER_TEXTBOX
+            // 
+            this.OPENER_TEXTBOX.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OPENER_TEXTBOX.Location = new System.Drawing.Point(402, 285);
+            this.OPENER_TEXTBOX.Name = "OPENER_TEXTBOX";
+            this.OPENER_TEXTBOX.Size = new System.Drawing.Size(92, 31);
+            this.OPENER_TEXTBOX.TabIndex = 47;
+            // 
+            // OPENER_QUADRANT_TEXTBOX
+            // 
+            this.OPENER_QUADRANT_TEXTBOX.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OPENER_QUADRANT_TEXTBOX.Location = new System.Drawing.Point(654, 285);
+            this.OPENER_QUADRANT_TEXTBOX.Name = "OPENER_QUADRANT_TEXTBOX";
+            this.OPENER_QUADRANT_TEXTBOX.Size = new System.Drawing.Size(92, 31);
+            this.OPENER_QUADRANT_TEXTBOX.TabIndex = 49;
+            // 
+            // OPENER_QUADRANT_LABEL
+            // 
+            this.OPENER_QUADRANT_LABEL.AutoSize = true;
+            this.OPENER_QUADRANT_LABEL.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OPENER_QUADRANT_LABEL.Location = new System.Drawing.Point(513, 282);
+            this.OPENER_QUADRANT_LABEL.Name = "OPENER_QUADRANT_LABEL";
+            this.OPENER_QUADRANT_LABEL.Size = new System.Drawing.Size(135, 31);
+            this.OPENER_QUADRANT_LABEL.TabIndex = 48;
+            this.OPENER_QUADRANT_LABEL.Text = "Quadrant:";
+            // 
+            // O_TWO_QUADRANT_TEXTBOX
+            // 
+            this.O_TWO_QUADRANT_TEXTBOX.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.O_TWO_QUADRANT_TEXTBOX.Location = new System.Drawing.Point(654, 344);
+            this.O_TWO_QUADRANT_TEXTBOX.Name = "O_TWO_QUADRANT_TEXTBOX";
+            this.O_TWO_QUADRANT_TEXTBOX.Size = new System.Drawing.Size(92, 31);
+            this.O_TWO_QUADRANT_TEXTBOX.TabIndex = 53;
+            // 
+            // O_TWO_QUADRANT_LABEL
+            // 
+            this.O_TWO_QUADRANT_LABEL.AutoSize = true;
+            this.O_TWO_QUADRANT_LABEL.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.O_TWO_QUADRANT_LABEL.Location = new System.Drawing.Point(513, 341);
+            this.O_TWO_QUADRANT_LABEL.Name = "O_TWO_QUADRANT_LABEL";
+            this.O_TWO_QUADRANT_LABEL.Size = new System.Drawing.Size(135, 31);
+            this.O_TWO_QUADRANT_LABEL.TabIndex = 52;
+            this.O_TWO_QUADRANT_LABEL.Text = "Quadrant:";
+            // 
+            // O_TWO_TEXTBOX
+            // 
+            this.O_TWO_TEXTBOX.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.O_TWO_TEXTBOX.Location = new System.Drawing.Point(402, 344);
+            this.O_TWO_TEXTBOX.Name = "O_TWO_TEXTBOX";
+            this.O_TWO_TEXTBOX.Size = new System.Drawing.Size(92, 31);
+            this.O_TWO_TEXTBOX.TabIndex = 51;
+            // 
+            // O_TWO_LABEL
+            // 
+            this.O_TWO_LABEL.AutoSize = true;
+            this.O_TWO_LABEL.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.O_TWO_LABEL.Location = new System.Drawing.Point(335, 344);
+            this.O_TWO_LABEL.Name = "O_TWO_LABEL";
+            this.O_TWO_LABEL.Size = new System.Drawing.Size(61, 31);
+            this.O_TWO_LABEL.TabIndex = 50;
+            this.O_TWO_LABEL.Text = "0-2:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(300, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(402, 37);
+            this.label1.TabIndex = 54;
+            this.label1.Text = "Mound Appearance Entry";
+            // 
+            // MoundAppearanceEntryScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.O_TWO_QUADRANT_TEXTBOX);
+            this.Controls.Add(this.O_TWO_QUADRANT_LABEL);
+            this.Controls.Add(this.O_TWO_TEXTBOX);
+            this.Controls.Add(this.O_TWO_LABEL);
+            this.Controls.Add(this.OPENER_QUADRANT_TEXTBOX);
+            this.Controls.Add(this.OPENER_QUADRANT_LABEL);
+            this.Controls.Add(this.OPENER_TEXTBOX);
+            this.Controls.Add(this.OPENER_LABEL);
+            this.Controls.Add(this.RIGHT_CHECKBOX);
+            this.Controls.Add(this.LEFT_CHECKBOX);
+            this.Controls.Add(this.BATTER_LABEL);
+            this.Controls.Add(this.SUBMIT_BUTTON);
+            this.Controls.Add(this.SELECT_PITCHER_LABEL);
+            this.Controls.Add(this.PITCHER_SELECT_COMBOBOX);
+            this.Name = "MoundAppearanceEntryScreen";
+            this.Size = new System.Drawing.Size(1011, 779);
+            this.Load += new System.EventHandler(this.MoundAppearanceEntryScreen_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SUBMIT_BUTTON;
+        private System.Windows.Forms.Label SELECT_PITCHER_LABEL;
+        private System.Windows.Forms.ComboBox PITCHER_SELECT_COMBOBOX;
+        private System.Windows.Forms.Label BATTER_LABEL;
+        private System.Windows.Forms.CheckBox LEFT_CHECKBOX;
+        private System.Windows.Forms.CheckBox RIGHT_CHECKBOX;
+        private System.Windows.Forms.Label OPENER_LABEL;
+        private System.Windows.Forms.TextBox OPENER_TEXTBOX;
+        private System.Windows.Forms.TextBox OPENER_QUADRANT_TEXTBOX;
+        private System.Windows.Forms.Label OPENER_QUADRANT_LABEL;
+        private System.Windows.Forms.TextBox O_TWO_QUADRANT_TEXTBOX;
+        private System.Windows.Forms.Label O_TWO_QUADRANT_LABEL;
+        private System.Windows.Forms.TextBox O_TWO_TEXTBOX;
+        private System.Windows.Forms.Label O_TWO_LABEL;
+        private System.Windows.Forms.Label label1;
+    }
+}
